574902576ac14a84824beafd0ee1d668830a2236
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-24 22:08:36 +0530

    Add:-Welcome Message

---
+++ src/com/Bridgelabs/AddressBook.java
@@ -0,0 +1,11 @@
+package com.Bridgelabs;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("*******************************************");
+		System.out.println(" Welcome to the address book program \n*******************************************");
+
+	}
+
+}
