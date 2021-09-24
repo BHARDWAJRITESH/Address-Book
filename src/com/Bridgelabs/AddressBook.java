0d9b085f87cde4e3532c7123fc689a9f0f4b4d57
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-24 23:55:32 +0530

    Add:-Ability to edit contact to address book

--- src/com/Bridgelabs/AddressBook.java
+++ src/com/Bridgelabs/AddressBook.java
@@ -1,12 +1,13 @@
 package com.Bridgelabs;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner; 
 
 public class AddressBook {
 	 	static ArrayList<Contact> contactList = new ArrayList<>();
 	
-	 	private void createContact() {
+	 	 void createContact() {
 	 		Scanner scan = new Scanner(System.in);
 
 	 		System.out.print("Enter First Name: ");
@@ -37,6 +38,50 @@ public class AddressBook {
 	 		contactList.add(contact);
 	 		System.out.println("\nContact Added.......\n");
 	 	}
+	 	
+	 	 void edit() {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter Name To Edit from Contact list: ");
+	        String FirstName = scanner.nextLine();
+	        for (Contact value : contactList) {
+	            if (Objects.equals(FirstName,value.firstName)) {
+	                System.out.print("Enter First Name: ");
+	                String firstName = scanner.nextLine();
+	                
+	                System.out.print("Enter Last Name: ");
+	                String lastName = scanner.nextLine();
+	                
+	                System.out.print("Enter Address: ");
+	                String address = scanner.nextLine();
+	               
+	                System.out.print("Enter City: ");
+	                String city = scanner.nextLine();
+	               
+	                System.out.print("Enter State: ");
+	                String state = scanner.nextLine();
+	                
+	                System.out.print("Enter ZipCode: ");
+	                String zipcode = scanner.nextLine();
+	                
+	                System.out.print("Enter Phone Number: ");
+	                String number = scanner.nextLine();
+	                
+	                System.out.print("Enter Email id: ");
+	                String email = scanner.nextLine();
+	               
+	                System.out.println("\n Success !  contact updated.....\n");
+
+	            } else System.out.println("The name entered is incorrect.");
+	            
+	        }
+	    }
+	 	 
+	 	 void showOutput() {
+	        System.out.println("*****************\n  Address List\n***************** ");
+	        for (Contact contact : contactList) {
+	            System.out.println(contact.toString());
+	        }
+	    }
 
 	 	public static void main(String[] args) {
 	 			System.out.println("*******************************************");
@@ -47,7 +92,7 @@ public class AddressBook {
 	 			while (choice == 0) {
 	 				
 	 				Scanner scan = new java.util.Scanner(System.in);
-	 				System.out.println("1. Add Contact \n2. Exit ");
+	 				System.out.println("1. Add Contact \n2. Edit \n3. Show Output \n4. Exit ");
 	 				int option = scan.nextInt();
 	 				
 	 				switch(option) {
@@ -55,10 +100,16 @@ public class AddressBook {
 	 					addressBook.createContact();
 	 					break;
 	 				case 2:
-	 					choice=1;
+	 					addressBook.edit();
 	 					break;
+	 				case 3:
+	 					addressBook.showOutput();
+	 					break;
+	 				case 4:
+	 					choice=1;
+	 					
 	 				default:
-	 					System.out.println("Please enter 1 or 2 only");
+	 					System.out.println("Please enter 1,2,3 or 4 only");
 	 				}
 	 			}
 	 	}
